f0bd9f7385d29d7e805a292faa198a514dfc2682
johnmarkbondad <johnmarkbondad333@gmail.com>
2023-07-06 17:09:21 +0800

    design nested stepper

--- src/views/Artist/EditProfile.vue
+++ src/views/Artist/EditProfile.vue
@@ -27,58 +27,58 @@
     <social-media />
     <form @submit.prevent="submit">
 
-    <input type="file" @input="form.avatar = $event.target.files[0]" accept="image/png, image/webp, image/svg, image/jpeg"/>
-    <div v-if="errors?.avatar">{{ errors.avatar }}</div>
+        <input type="file" @input="form.avatar = $event.target.files[0]" accept="image/png, image/webp, image/svg, image/jpeg"/>
+        <div v-if="errors?.avatar">{{ errors.avatar }}</div>
 
-    <progress v-if="form.progress" :value="form.progress.percentage" max="100">{{ form.progress.percentage }}%</progress>
+        <progress v-if="form.progress" :value="form.progress.percentage" max="100">{{ form.progress.percentage }}%</progress>
 
-    <select v-model="form.artist_type">
-    <option v-for="artist_type in artist_types" :key="artist_type.id" :value="artist_type.title">{{ artist_type.title }}</option>
-    </select>
-    <div v-if="errors?.artist_type">{{ errors.artist_type }}</div>
+        <select v-model="form.artist_type">
+        <option v-for="artist_type in artist_types" :key="artist_type.id" :value="artist_type.title">{{ artist_type.title }}</option>
+        </select>
+        <div v-if="errors?.artist_type">{{ errors.artist_type }}</div>
 
-    <input type="text" v-model="form.artist_name" placeholder="Name of the Artist/Band" />
-    <div v-if="errors?.artist_name">{{ errors.artist_name }}</div>
+        <input type="text" v-model="form.artist_name" placeholder="Name of the Artist/Band" />
+        <div v-if="errors?.artist_name">{{ errors.artist_name }}</div>
 
-    <multi-select
-    v-model="form.genre"
-    :options="genres"
-    mode="tags"
-    placeholder="Please select genres"
-    class="my-5"
-    />
-    <div v-if="errors?.genre">{{ errors.genre }}</div>
+        <multi-select
+        v-model="form.genre"
+        :options="genres"
+        mode="tags"
+        placeholder="Please select genres"
+        class="my-5"
+        />
+        <div v-if="errors?.genre">{{ errors.genre }}</div>
 
-    <ul class="list-group" v-if="members">
-    <li class="list-group-item" v-for="mem in members" :key="mem.id">
-    <b-avatar>
-    <template #default>
-    <span class="b-avatar-img">
-    <img loading="lazy" :src="`/storage/${mem.avatar}`" alt="" v-if="mem.avatar">
-    <!-- <font-awesome-icon icon="fa-solid fa-bomb" /> -->
-    <span v-else>
-        {{ mem.avatar_text }}
-    </span>
+        <ul class="list-group" v-if="members">
+        <li class="list-group-item" v-for="mem in members" :key="mem.id">
+        <b-avatar>
+        <template #default>
+        <span class="b-avatar-img">
+        <img loading="lazy" :src="`/storage/${mem.avatar}`" alt="" v-if="mem.avatar">
+        <!-- <font-awesome-icon icon="fa-solid fa-bomb" /> -->
+        <span v-else>
+            {{ mem.avatar_text }}
+        </span>
 
-    </span>
-    </template>
-    </b-avatar>
-    <h6 class="card-title">{{ mem.fullname }}</h6>
-    <p class="card-text">{{ mem.role }}</p>
-    <Link :href="`/artist/profile/member/${mem.id}`" method="delete" class="btn btn-danger">
-    <font-awesome-icon icon="fa-solid fa-trash" />
-    </Link>
-    <Link href="" >Edit</Link>
+        </span>
+        </template>
+        </b-avatar>
+        <h6 class="card-title">{{ mem.fullname }}</h6>
+        <p class="card-text">{{ mem.role }}</p>
+        <Link :href="`/artist/profile/member/${mem.id}`" method="delete" class="btn btn-danger">
+        <font-awesome-icon icon="fa-solid fa-trash" />
+        </Link>
+        <Link href="" >Edit</Link>
 
-    </li>
+        </li>
 
-    </ul>
+        </ul>
 
-    <member-form />
-    <textarea v-model="form.bio" ></textarea>
-    <div v-if="errors?.bio">{{ errors.bio }}</div>
+        <member-form />
+        <textarea v-model="form.bio" ></textarea>
+        <div v-if="errors?.bio">{{ errors.bio }}</div>
 
-    <button type="submit" class="btn-success btn">Submit</button>
+        <button type="submit" class="btn-success btn">Submit</button>
     </form>
   </div>
 </template>
@@ -112,15 +112,7 @@ export default {
   },
   mounted()
   {
-    this.fetchArtistOptions().catch(err =>
-    {
-      this.$vs.notification({
-        color: 'danger',
-        position: 'top-right',
-        title: 'Server Status',
-        text: `${err.message}`
-      })
-    });
+    this.fetchArtistOptions()
   },
   props: {
     errors: Object,
@@ -144,16 +136,8 @@ export default {
           this.$router.push("/artist");
         }
 
-      })
-      .catch(err =>
-      {
-        this.$vs.notification({
-          color: 'danger',
-          position: 'top-right',
-          title: 'Server Status',
-          text: `${err.message}`
-        })
       });
+
     }
   },
   computed: {
