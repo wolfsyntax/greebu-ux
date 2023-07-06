44c84616e69d9f5388a8b2ca8f279765232dcb18
johnmarkbondad <johnmarkbondad333@gmail.com>
2023-07-06 16:56:51 +0800

    design nested stepper

--- src/views/BasicInformation/Index.vue
+++ src/views/BasicInformation/Index.vue
@@ -6,16 +6,14 @@
 
     <div class="row">
       <div class="col-md-6 offset-md-3">
-        <div class="d-flex justify-content-center">
+        <div class="progress">
+          <div class="progress-bar" :style="{ width: progressWidth }"></div>
+        </div>
+        <div class="d-flex justify-content-center mb-3">
           <div v-for="(step, index) in steps" :key="index" :class="['step-item', { 'active': index === currentStep }]">
-            <div class="step-number">{{ index + 1 }}</div>
             <div class="step-title">{{ step.title }}</div>
           </div>
         </div>
-        <!-- <div class="step-content" v-for="(step, index) in steps" :key="index" :class="{'active': index === currentStep}">
-          <h3>{{ step.title }}</h3>
-          <p>{{ step.content }}</p>
-        </div> -->
 
         <div class="row step1" v-if="currentStep === 0">
           <div class="card">
@@ -38,77 +36,136 @@
                   </div>
                   <div class="button-wrapper">
                   <button type="button" class="btn btn-primary back" @click="previousStep1" :disabled="currentStep === 0">Back</button>
-                  <button type="button" class="btn btn-primary next" @click="nextStep" :disabled="currentStep === steps.length - 1">Next</button>
+                  <button type="button" class="btn btn-primary next" @click="nextStep">Next</button>
                 </div>
                </form>
             </div>
           </div>
         </div> <!-- end of step1 row -->
 
-        <div class="row step3" v-if="currentStep === 2">
-          <div class="card">
-            <div class="card-body">
-              <h2 class="card-title">How the song make them feel?</h2>
-              <form @submit.prevent="submit">
-                  <div class="form-group">
-                    <label for="mood">Select mood</label>
-                    <input id="mood" type="text" class="form-control" name="mood" placeholder="Happy">
-                  </div>
-                  <div class="button-wrapper">
-                  <button type="button" class="btn btn-primary back" @click="previousStep" :disabled="currentStep === 0">Back</button>
-                  <button type="button" class="btn btn-primary next" @click="nextStep" :disabled="currentStep === steps.length - 1">Next</button>
+        <div class="row step2" v-if="currentStep === 1">
+          <div class="step-content" v-for="(step, index) in steps" :key="index" :class="{'active': index === currentStep}">
+          <div v-if="index === 1">
+            <div class="progress">
+              <div class="progress-bar" :style="{ width: subProgressWidth }"></div>
+            </div>
+            <div class="d-flex justify-content-center">
+              <div v-for="(subStep, subIndex) in subSteps" :key="subIndex" :class="['step-item', { 'active': subIndex === currentSubStep }]">
+                <div class="step-title">{{ subStep.title }}</div>
+              </div>
+            </div>
+                                                           <!-- SONG substeps -->
+            <div v-if="currentSubStep === 0">
+                <div class="col-12">
+                  <h2 class="title">Select your artist</h2>
+                  <p class="sub-title">These artists are available now! Tap an artist to hear a sample song that showcases their style and voice.</p>
+                      <div class="button-wrapper">
+                      <button type="button" class="btn btn-primary back" @click="previousStep" :disabled="currentStep === 0">Back</button>
+                      <button type="button" class="btn btn-primary next" @click="subNextStep">Next</button>
+                    </div>
                 </div>
-               </form>
+            </div>
+            <div v-if="currentSubStep === 1">
+              <div class="card">
+                  <div class="card-body">
+                    <h2 class="card-title">How the song make them feel?</h2>
+                    <form @submit.prevent="submit">
+                        <div class="form-group">
+                          <label for="mood">Select mood</label>
+                            <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              <span :style="selectedMood ? { color: '#FF6B00' } : {}">{{ selectedMood ? selectedMood.name : 'Select mood' }}</span>
+                              <img :src="expandMore.img" :alt="expandMore.altText">
+                            </button>
+                            <ul class="dropdown-menu">
+                              <li v-for="option in moodOptions" :key="option.id" @click="selectMood(option)">
+                                {{ option.name }}
+                              </li>
+                            </ul>
+                          </div>
+                        </div> 
+                        <div class="button-wrapper">
+                        <button type="button" class="btn btn-primary back" @click="subPreviousStep" :disabled="currentStep === 0">Back</button>
+                        <button type="button" class="btn btn-primary next" @click="subNextStep" :disabled="!selectedMood">Next</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+            </div>
+            <div v-if="currentSubStep === 2">
+              <div class="card">
+                <div class="card-body">
+                  <h2 class="card-title">More Details substep</h2>
+                  <form @submit.prevent="submit">
+                      <div class="form-group">
+                        <label for="language">Select Language</label>
+                          <div class="dropdown">
+                          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span :style="selectedLanguage ? { color: '#FF6B00' } : {}">{{ selectedLanguage ? selectedLanguage.name : 'Select Language' }}</span>
+                            <img :src="expandMore.img" :alt="expandMore.altText">
+                          </button>
+                          <ul class="dropdown-menu">
+                            <li v-for="language in languageOptions" :key="language.id" @click="selectLanguage(language)">
+                              {{ language.name }}</li>
+                          </ul>
+                        </div>
+                      </div> 
+                      <div class="form-group">
+                        <label for="language">Duration of Song</label>
+                          <div class="dropdown">
+                          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span :style="selectedSongDuration ? { color: '#FF6B00' } : {}">{{ selectedSongDuration ? selectedSongDuration.time : 'Select Duration of Song' }}</span>
+                            <img :src="expandMore.img" :alt="expandMore.altText">
+                          </button>
+                          <ul class="dropdown-menu">
+                            <li v-for="duration in songDurationOptions" :key="duration.id" @click="selectSongDuration(duration)">
+                              {{ duration.time }}</li>
+                          </ul>
+                        </div>
+                      </div> 
+                      <div class="button-wrapper">
+                      <button type="button" class="btn btn-primary back" @click="subPreviousStep" :disabled="currentStep === 0">Back</button>
+                      <button type="button" class="btn btn-primary next" @click="nextStep" :disabled="!(selectedLanguage && selectedSongDuration)">Next</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+        </div>
+
+        <div class="row step3" v-if="currentStep === 2">
+                   Step 3 - STORY   -  Occasion Details
+                   <div class="button-wrapper">
+                  <button type="button" class="btn btn-primary back" @click="previousStep">Back</button>
+                  <button type="button" class="btn btn-primary next" @click="nextStep">Next</button>
+                </div>
+        </div>
 
         <div class="row step4" v-if="currentStep === 3">
-          <div class="card">
-            <div class="card-body">
-              <h2 class="card-title">More Details</h2>
-              <form @submit.prevent="submit">
-                  <div class="form-group">
-                    <label for="mood">Select Language</label>
-                    <select class="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">English</option>
-                    <option value="2">Bikol</option>
-                    <option value="3">Tagalog</option>
-                  </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="mood">Duration of Song</label>
-                    <select class="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">5 minutes</option>
-                    <option value="2">10 minutes</option>
-                    <option value="3">15 minutes</option>
-                  </select>
-                  </div>
-                  <div class="button-wrapper">
-                  <button type="button" class="btn btn-primary back" @click="previousStep" :disabled="currentStep === 0">Back</button>
-                  <button type="button" class="btn btn-primary next" @click="nextStep" :disabled="currentStep === steps.length - 1">Next</button>
+                          Step 4 - REVIEW
+                          <div class="button-wrapper">
+                  <button type="button" class="btn btn-primary back" @click="previousStep">Back</button>
+                  <button type="button" class="btn btn-primary next" @click="nextStep">Next</button>
                 </div>
-               </form>
-            </div>
-          </div>
         </div>
 
       </div>
     </div> <!-- end of top row -->
 
-        <div class="row step2" v-if="currentStep === 1">
+        <!-- <div class="row step2" v-if="currentStep === 1">
+
+
           <div class="col-12">
             <h2 class="title">Select your artist</h2>
             <p class="sub-title">These artists are available now! Tap an artist to hear a sample song that showcases their style and voice.</p>
                 <div class="button-wrapper">
                 <button type="button" class="btn btn-primary back" @click="previousStep" :disabled="currentStep === 0">Back</button>
-                <button type="button" class="btn btn-primary next" @click="nextStep" :disabled="currentStep === steps.length - 1">Next</button>
+                <button type="button" class="btn btn-primary next" @click="nextStep">Next</button>
               </div>
           </div>
-
-        </div>
+        </div> -->
 
 
         <!-- <div class="button-container">
@@ -143,14 +200,59 @@
             { title: 'STORY', content: 'STORY' },
             { title: 'REVIEW', content: 'REVIEW' }
           ],
-          currentStep: 0
+          subSteps: [
+          { title: 'Select your artist'},
+            { title: 'Select mood' },
+            { title: 'Select language' }
+          ],
+
+      currentStep: 0,
+      currentSubStep: 0,
+      selectedMood: null,
+      moodOptions: [
+      { id: 1, name: 'Happy' },
+      { id: 2, name: 'Surprise' },
+      { id: 3, name: 'Anger' }
+        ],
+      selectedLanguage: null,
+      languageOptions: [
+        { id: 1, name: 'English' },
+        { id: 2, name: 'Tagalog' },
+        { id: 3, name: 'Taglish' },
+        { id: 4, name: 'Bikol' }
+        ],
+      selectedSongDuration: null, 
+      songDurationOptions: [
+      { id: 1, time: '5 minutes' },
+      { id: 2, time: '10 minutes' },
+      { id: 3, time: '15 minutes' }
+      ],
+      expandMore: {
+        img: 'src/assets/expand-more.svg',
+        altText: 'expand icon to see list of items in the dropdown menu'
+      },
+      currentStep: 0
+
         };
+      },
+      computed: {
+        progressWidth() {
+          return ((this.currentStep + 1) / this.steps.length) * 100 + '%';
+        },
+        subProgressWidth() {
+          return ((this.currentSubStep + 1) / this.subSteps.length) * 100 + '%';
+        }
       },
       methods: {
         nextStep() {
           if (this.currentStep < this.steps.length - 1) {
             this.currentStep++;
           }
+        },
+        subNextStep(){
+          if (this.currentSubStep < this.subSteps.length - 1) {
+            this.currentSubStep++;
+          } 
         },
         previousStep() {
           if (this.currentStep > 0) {
@@ -159,10 +261,24 @@
             this.$router.push('/subscription');
           }
         },
+        subPreviousStep(){
+          if (this.currentSubStep > 0) {
+            this.currentSubStep--;
+          } 
+        },
         previousStep1(){
          if (this.currentStep === 0) {
             window.location.href = '/create-song';
         }
+      },
+      selectMood(mood) {
+      this.selectedMood = mood;
+      },
+      selectLanguage(language) {
+      this.selectedLanguage = language;
+      },
+      selectSongDuration(duration){
+        this.selectedSongDuration = duration;
       }
       },
    components: {
@@ -172,6 +288,17 @@
  </script>
 
  <style scoped>
+    .progress {
+      height: 10px;
+      background-color: #ddd;
+      border-radius: 10px;
+    }
+    
+    .progress-bar {
+      background-color: orange;
+      border-radius: 10px;
+    }
+    
     .step-item {
       flex: 1;
       display: flex;
@@ -179,22 +306,7 @@
       justify-content: center;
       position: relative;
     }
-    
-    .step-item::after {
-      content: "";
-      position: absolute;
-      top: 50%;
-      left: 100%;
-      height: 2px;
-      width: 100%;
-      background-color: #ddd;
-      transform: translateY(-50%);
-    }
-    
-    .step-item:last-child::after {
-      display: none;
-    }
-    
+
     .step-number {
       width: 30px;
       height: 30px;
@@ -226,7 +338,7 @@
     .button-container {
       margin-top: 20px;
       display: flex;
-      justify-content: flex-end;
+      justify-content: space-between;
     }
 </style>
 
