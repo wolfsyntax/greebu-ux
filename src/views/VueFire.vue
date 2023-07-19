72c1979cfd10020f93ad25bb52f8087c4238d737
Wolf Syntax <jaysonalpe@gmail.com>
2023-07-19 13:44:34 +0800

    Firebase Authentication using VueFire instead of firebase

---
+++ src/views/VueFire.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="signIn">Google Login</button>
+  </div>
+</template>
+
+<script>
+import
+  {
+    getRedirectResult,
+    signInWithRedirect,
+    signOut,
+    signInWithPopup,
+    GoogleAuthProvider
+  } from 'firebase/auth'
+import { useCurrentUser, useFirebaseAuth } from 'vuefire';
+export const googleAuthProvider = new GoogleAuthProvider();
+
+export default {
+  setup () {
+    const auth = useFirebaseAuth()
+    function signIn()
+    {
+      signInWithPopup(auth, googleAuthProvider).catch((reason) =>
+      {
+        console.error('Failed sign', reason)
+        error.value = reason
+      })
+    }
+    return {
+      signIn
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
