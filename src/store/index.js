ed52baaf73be317e0ae7d8508bbd1b612dbcdc06
Wolf Syntax <jaysonalpe@gmail.com>
2023-08-25 14:34:09 +0800

    Vuex persisted state with window.sessionStorage

--- src/store/index.js
+++ src/store/index.js
@@ -16,17 +16,19 @@ import customer from './modules/customers';
 import organizer from './modules/organizers';
 import services from './modules/services';
 import songs from './modules/songs';
+// json-cookie
+import * as Cookies from 'js-cookie';
 
-const ls = new SecureLS({ isCompression: false });
+// const ls = new SecureLS({ isCompression: false });
 
 export default new Vuex.Store({
     plugins: [
     createPersistedState({
-      storage: {
-        getItem: (key) => ls.get(key),
-        setItem: (key, value) => ls.set(key, value),
-        removeItem: (key) => ls.remove(key)
-      }
+      storage: window.sessionStorage, /*{
+        // getItem: (key) => ls.get(key),
+        // setItem: (key, value) => ls.set(key, value),
+        // removeItem: (key) => ls.remove(key)
+      }*/
     }),
     createMutationsSharer({
       predicate: (mutation, state) => {
