c887c3ab9d361eaa806fe5883c7aca5fe3d5dca2
Wolf Syntax <jaysonalpe@gmail.com>
2023-08-28 14:52:53 +0800

    Update for artist genre not displayed in multiselect

--- src/store/modules/artists/getters.js
+++ src/store/modules/artists/getters.js
@@ -2,3 +2,8 @@
 export const stepOne = (state,getters, rootState) => {}
 export const artistProfile = state => state.artist
 export const artistGenre = state => state.artist_genres
+
+export const formArtistGenres = state => state.genres.concat(state.artist_genres.filter(el =>
+{
+  return !state.genres.includes(el);
+}))
