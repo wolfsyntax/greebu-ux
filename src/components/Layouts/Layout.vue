18ac96c2b0f1a931e32d061847074397e77d8dfe
johnmarkbondad <johnmarkbondad333@gmail.com>
2023-08-29 17:48:28 +0800

    design song preview

--- src/components/Layouts/Layout.vue
+++ src/components/Layouts/Layout.vue
@@ -27,9 +27,9 @@
               <a class="nav-link" :href="artist.link">{{ artist.page }}</a>
               </li>
 
-              <!-- <li class="nav-item" v-for="(organizer, index) in navItemsOrganizers" :key="index" v-if="userRole === 'organizers'">
+              <li class="nav-item" v-for="(organizer, index) in navItemsOrganizers" :key="index" v-if="userRole === 'organizer'">
               <a class="nav-link" :href="organizer.link">{{ organizer.page }}</a>
-              </li> -->
+              </li>
 
             </ul>
 
@@ -38,10 +38,6 @@
               <a :href="signUp" type="button" class="btn btn-secondary sign-up">Sign Up</a>
             </div>
             <div class="float-end nav-button" v-else>
-              <!-- <h2 v-if="userRole === 'artists'">Artist</h2>
-              <h2 v-else-if="userRole === 'customers'">Customer</h2>
-              <h2 v-else-if="userRole === 'organizers'">Organizer</h2> -->
-
                                                   <!-- CUSTOMERS MENU AND DROPDOWN -->
 
            <div v-if="userRole === 'customers'" class="d-flex align-items-center customers">
@@ -123,6 +119,45 @@
                 </ul>
               </div> <!-- end of dropdown -->
             </div> <!-- end of user role for artists -->
+
+                                                           <!-- ORGANIZERS MENU AND DROPDOWN -->
+
+           <div v-if="userRole === 'organizer'" class="d-flex align-items-center artists">
+              <a href="#"><span class="material-symbols-outlined bell-icon">&#xe7f4;</span></a>
+
+               <div class="dropdown dropstart">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                  <img :src="myAvatar" alt="artist profile">
+                 <span class="material-symbols-rounded">&#xe313;</span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+                  <li>
+                    <div class="artist-info">
+                      <img :src="myAvatar" alt="artist profile">
+                      <div class="artist-name">
+                        <p class="name">{{ userInfo.business_name }}</p>
+                        <p class="email">{{ userInfo.role }}</p>
+                        <a class="dropdown-item view-profile" href="/artist">View Profile</a>
+                      </div>
+                    </div>
+                  </li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li v-for="(organizer, index) in organizersDropdown" :key="index">
+                    <span class="material-symbols-outlined">{{ organizer.icon }}</span>
+                    <a class="dropdown-item" :href="organizer.link">{{ organizer.name }}</a>
+                  </li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li class="logout-wrapper">
+                    <span class="material-symbols-outlined">&#xe040;</span>
+                    <a class="dropdown-item logout" href="#" @click.prevent="logout">Switch Account</a>
+                  </li>
+                  <li class="logout-wrapper">
+                    <span class="material-symbols-rounded">logout</span>
+                    <a class="dropdown-item logout" href="#" @click.prevent="logout">Logout</a>
+                  </li>
+                </ul>
+              </div> <!-- end of dropdown -->
+            </div> <!-- end of user role for organizers -->
 
 
             </div> <!-- end v-else -->
@@ -247,6 +282,13 @@ export default {
         { page: 'Organizers', link: '/organizers' },
         { page: 'Events', link: '/events' }
       ],
+      navItemsOrganizers: [
+        { page: 'Home', link: '/' },
+        { page: 'Artist', link: '/artists' },
+        { page: 'Organizers', link: '/organizers' },
+        { page: 'Events', link: '/events' },
+        { page: 'Reports', link: '/reports' }
+      ],
       customersDropdown: [
         // change all icons name to &#x + Code point
         { icon: 'account_circle', name: 'Account Settings', link: ''},
@@ -264,6 +306,13 @@ export default {
         // { icon: 'queue_music', name: 'My Proposals', link: '/'},
         { icon: 'upgrade', name: 'My Subscription', link: '/'},
         // { icon: 'help', name: 'Help Center', link: '/'},
+      ],
+      organizersDropdown: [
+        { icon: 'account_circle', name: 'Account Settings', link: '/account/setting'},
+        { icon: 'mail', name: 'Message', link: '/message'},
+        { icon: 'event_available', name: 'My Bookings', link: '/'},
+        // { icon: 'queue_music', name: 'My Proposals', link: '/'},
+        { icon: 'help', name: 'Help Center', link: '/'},
       ],
       home: '/',
       createSong: '/create-song',
