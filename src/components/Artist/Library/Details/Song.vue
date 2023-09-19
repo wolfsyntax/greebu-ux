1f17dce6f345540d3f319323aac3778d361f0ca5
Wolf Syntax <jaysonalpe@gmail.com>
2023-09-19 20:16:22 +0800

    Initialize library page

---
+++ src/components/Artist/Library/Details/Song.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <form @submit.prevent="submit">
+      <div class="mb-3">
+        
+        <label for="songTitle" class="form-label">Title of Song</label>
+        <span class="badge text-bg-primary float-end">Required</span>
+        <input type="text" class="form-control" id="songTitle" v-model="form.song_title" required>
+      </div>
+
+      <div class="mb-3">
+        <label for="songComposer" class="form-label">Composer</label>
+        <span class="badge text-bg-primary float-end">Required</span>
+        <input type="text" class="form-control" id="songComposer" v-model="form.composer" required>
+      </div>
+
+      <div class="mb-3">
+        <label for="songGenre" class="form-label">Genre</label>
+        <span class="badge text-bg-primary float-end">Required</span>
+        <select v-model="form.genre" required class="form-select">
+          <option selected></option>
+          <option v-for="(gen, index) in genres" :key="index" :value="gen">
+            {{ gen }}
+          </option>
+        </select>
+      </div>
+      
+      <div class="float-end">
+        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Next</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState, mapActions, mapMutations } from "vuex";
+
+export default {
+  components:
+  {
+
+  },
+  setup () {
+    
+
+    return {}
+  },
+  data: () => ({
+    form: {
+      song_title: '',
+      composer: '',
+      genre: '',
+    }
+  }),
+  methods: {
+    submit()
+    {
+      this.$emit('step', {})
+    }
+  },
+  mounted()
+  {
+
+  },
+  computed: {
+    ...mapState({
+      ...mapGetters(['isLoggedIn',]),
+      genres: (state) => state.artist.genres,
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
