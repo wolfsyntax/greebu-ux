1f026cb7b5f96eeae2f8760fe9621fd4f5492cfe
Wolf Syntax <jaysonalpe@gmail.com>
2023-10-05 17:21:04 +0800

    Restore viewing event details

--- src/components/Events/Card.vue
+++ src/components/Events/Card.vue
@@ -1,66 +1,58 @@
 <template>
   <div class="card">
-
     <div class="bg-wrapper">
-      <img :src="event.cover_photo" class="img-fluid card-bg" loading="lazy" alt="Event image">
-      <div class="d-flex align-items-start organized-by">
-        <img :src="event.organizer_avatar" class="float-start" alt="Organized by logo">
+      <img :src="event?.cover_photo" class="img-fluid card-bg" loading="lazy" alt="Event image">
+      <div class="organized-by">
+        <img :src="event?.organizer_avatar" class="float-start" alt="Organized by logo">
         <div>
-          <h5>{{ event.organizer_name }}</h5>
-          <p class="mb-0">Organized by</p>
+          <h5>{{ event?.organizer_name }}</h5>
+          <p>Organized by</p>
         </div>
       </div>
     </div> <!-- end of bg-wrapper -->
 
     <div class="card-body">
+      <h5 class="card-title">{{ event?.event_name }}
+        <span v-if="event?.is_free" class="badge free">Free</span>
+        <span v-else class="badge paid">Paid</span>
+      </h5>
 
-      <div class="event-title-wrap">
-        <h5 class="card-title">{{ event.event_name }} </h5>
-        <p class="mb-0 card-text">{{ event.description }}</p>
-      </div>
+      <p class="card-text">{{ event?.description }}</p>
+      <p class="location">
+        <span class="material-symbols-outlined">location_on</span>
+        <span class="text">{{ event?.location }}</span>
+      </p>
 
-      <div class="place-time-wrap">
+      <p class="date">
+        <span class="material-symbols-outlined">calendar_month</span>
+        <span class="orange-text">{{ $moment(`${event?.start_date}`).format('MMMM Do, YYYY') }}&nbsp;&mdash;&nbsp;{{
+          $moment(`${event?.end_date}`).format('MMMM Do, YYYY') }}</span>
+      </p>
 
-        <div class="location">
-          <span class="material-symbols-outlined">&#xe0c8;</span>
-          <p>{{ event.location }}</p>
-        </div>
-        <div class="date">
-          <span class="material-symbols-outlined">&#xebcc;</span>
-          <p>{{ $moment(`${event.start_date}`).format('MMMM Do, YYYY') }} - {{ $moment(`${event.end_date}`).format('MMMM Do, YYYY') }}</p>
-        </div>
-        <div class="time">
-          <span class="material-symbols-outlined">&#xe8b5;</span>
-          <p>{{ $moment(`${$moment().format('YYYY-MM-DD')} ${event.start_time}`).format('h:mm a') }} - {{
-            $moment(`${$moment().format('YYYY-MM-DD')} ${event.end_time}`).format('h:mm a') }}</p>
-        </div>
+      <p class="time">
+        <span class="material-symbols-outlined">schedule</span>
+        <span class="orange-text">{{ $moment(`${$moment().format('YYYY-MM-DD')} ${event?.start_time}`).format('h:mm a')
+        }}&nbsp;&mdash;&nbsp;{{ $moment(`${$moment().format('YYYY-MM-DD')} ${event?.end_time}`).format('h:mm a')
+}}</span>
+      </p>
 
-      </div>
-
-      <div class="seeking-for" v-if="(userRole === 'artists' || userRole === 'organizer') && event.look_types.length > 0">
+      <div class="seeking-for" v-if="(userRole === 'artists' || userRole === 'organizer') && event?.look_types.length > 0">
         <h6 class="title">Seeking for</h6>
-        <span class="badge type-artist" v-for="(look, index) in event.look_types" :key="index">{{ look }}</span>
-      </div>
-      <div class="seeking-for"
-        v-if="(userRole === 'artists' || userRole === 'organizer') && event.look_types.length === 0">
-        <h6 class="title" style="opacity: 0;">Seeking for</h6>
-        <span class="badge type-artist" style="opacity: 0;">acoustic band</span>
+        <span class="badge type-artist" v-for="(look, index) in event?.look_types" :key="index">{{ look }}</span>
       </div>
 
       <div v-if="isLoggedIn">
-        <button class="btn btn-primary view-details" data-bs-toggle="modal" data-bs-target="#eventDetailsModal">View
-          Details</button>
-        <viewEventDetails />
-      </div>
-      <div v-else>
-        <button class="btn btn-primary view-details" data-bs-toggle="modal" data-bs-target="#mustSignUp">View
-          Details</button>
-        <signupmodal />
+        <button class="btn btn-primary view-details" @click="toggle(pos)">View Details</button>
       </div>
 
-      <button class="btn btn-primary send-proposal" @click="sendProposal" v-if="userRole === 'artists'" >Send Proposal</button>
+      <div v-else>
+        <button class="btn btn-primary view-details" @click="toggle">View Details</button>
+      </div>
+
+      <button class="btn btn-primary send-proposal" @click="$router.push('/proposal')" v-if="userRole === 'artists'">Send
+        Proposal</button>
     </div>
-  </div> <!-- end of card -->
+  </div>
 </template>
 
 <script>
@@ -68,7 +60,8 @@ import { mapGetters, mapState, mapActions, mapMutations } from "vuex";
 import ViewDetail from '/src/components/Events/ViewEventDetailsModal.vue';
 import MustSignupModal from '/src/components/Artist/MustSignupModal.vue';
 export default {
-  setup() {
+  setup()
+  {
 
 
     return {}
@@ -93,11 +86,13 @@ export default {
   computed: {
     ...mapGetters(["isLoggedIn", 'userRole']),
   },
-  mounted() {
+  mounted()
+  {
     console.log('Card: ', this.event);
   },
   methods: {
-    toggle(pos = -1) {
+    toggle(pos = -1)
+    {
       if (pos > -1) {
         this.$store.commit('SET_EVENT', this.event);
       } else {
@@ -106,11 +101,6 @@ export default {
 
       this.$emit('show', pos);
 
-    },
-    sendProposal()
-    {
-      this.$store.commit('SET_EVENT', this.event);
-      this.$router.push('/proposal')
     }
   }
 }
