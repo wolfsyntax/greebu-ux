475e27fc90e57ba51803e295bd2b16c218f0eb9e
Wolf Syntax <jaysonalpe@gmail.com>
2023-10-05 11:56:02 +0800

    Update for viewing event details

---
+++ src/components/Events/Card.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="card">
+    <div class="bg-wrapper">
+      <img :src="event.cover_photo" class="img-fluid card-bg" loading="lazy" alt="Event image">
+      <div class="organized-by">
+        <img :src="event.organizer_avatar" class="float-start" alt="Organized by logo">
+        <div>
+          <h5>{{ event.organizer_name }}</h5>
+          <p>Organized by</p>
+        </div>
+      </div>
+    </div> <!-- end of bg-wrapper -->
+
+    <div class="card-body">
+      <h5 class="card-title">{{ event.event_name }} 
+        <span v-if="event.is_free" class="badge free">Free</span>
+        <span v-else class="badge paid">Paid</span>
+      </h5>
+
+      <p class="card-text">{{ event.description }}</p>
+      <p class="location">
+        <span class="material-symbols-outlined">location_on</span>
+        <span class="text">{{ event.location }}</span>
+      </p>
+
+      <p class="date">
+        <span class="material-symbols-outlined">calendar_month</span>
+        <span class="orange-text">{{ $moment(`${event.start_date}`).format('dddd, MMMM Do, YYYY') }}&nbsp;&mdash;&nbsp;{{ $moment(`${event.end_date}`).format('dddd, MMMM Do, YYYY') }}</span>
+      </p>
+
+      <p class="time">
+        <span class="material-symbols-outlined">schedule</span>
+        <span class="orange-text">{{ $moment(`${$moment().format('YYYY-MM-DD')} ${event.start_time}`).format('h:mm a') }}&nbsp;&mdash;&nbsp;{{ $moment(`${$moment().format('YYYY-MM-DD')} ${event.end_time}`).format('h:mm a') }}</span>
+      </p>
+
+      <div class="seeking-for" v-if="(userRole === 'artists' || userRole === 'organizer') && event.look_types.length > 0" >
+        <h6 class="title">Seeking for</h6>
+        <span class="badge type-artist" v-for="(look, index) in event.look_types" :key="index">{{ look }}</span>
+      </div>
+
+      <div v-if="isLoggedIn">
+        <button class="btn btn-primary view-details" @click="toggle(pos)">View Details</button>
+      </div>
+      
+      <div v-else>
+        <button class="btn btn-primary view-details" @click="toggle">View Details</button>
+      </div>
+
+      <button class="btn btn-primary send-proposal" @click="$router.push('/proposal')" v-if="userRole === 'artists'" >Send Proposal</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState, mapActions, mapMutations } from "vuex";
+import ViewDetail from '/src/components/Events/ViewEventDetailsModal.vue';
+import MustSignupModal from '/src/components/Artist/MustSignupModal.vue';
+export default {
+  setup () {
+    
+
+    return {}
+  },
+  components: {
+    ViewDetail,
+    signupmodal: MustSignupModal,
+  },
+  props: {
+    pos: { 
+      type: Number,
+      default: -1,
+      required: true
+    },
+
+    event: { 
+      type: Object,
+      default: {},
+      required: true
+    },
+  },
+  computed: {
+    ...mapGetters(["isLoggedIn", 'userRole']),
+  },
+  mounted()
+  {
+    console.log('Card: ', this.event);
+  },
+  methods: {
+    toggle(pos = -1)
+    {
+      if (pos > -1) {
+        this.$store.commit('SET_EVENT', this.event);
+      } else {
+        this.$store.commit('SET_EVENT', {});
+      }
+
+      this.$emit('show', pos);
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Homepage/Event.vue
+++ src/views/Homepage/Event.vue
@@ -91,54 +91,7 @@
         <!-- Upcoming Events -->
         <div class="row" v-if="events.length">
           <div class="col-3" v-for="(event, index) in events" :key="index">
-            <div class="card">
-              <div class="bg-wrapper">
-                <img :src="event.cover_photo" class="img-fluid card-bg" loading="lazy" alt="Event image">
-                <div class="organized-by">
-                <img :src="event.organizer_avatar" class="float-start" alt="Organized by logo">
-                <div>
-                  <h5>{{ event.organizer_name }}</h5>
-                  <p>Organized by</p>
-                </div>
-              </div>
-              </div> <!-- end of bg-wrapper -->
-              <div class="card-body">
-                <h5 class="card-title">{{ event.event_name }} 
-                  <span v-if="event.is_free" class="badge free">Free</span>
-                  <span v-else class="badge paid">Paid</span>
-                </h5>
-                <p class="card-text">{{ event.description }}</p>
-                <p class="location">
-                  <span class="material-symbols-outlined">location_on</span>
-                  <span class="text">{{ event.location }}</span>
-                </p>
-                <p class="date">
-                  <span class="material-symbols-outlined">calendar_month</span>
-                  <span class="orange-text">{{ $moment(`${event.start_date}`).format('dddd, MMMM Do, YYYY') }}&nbsp;&mdash;&nbsp;{{ $moment(`${event.end_date}`).format('dddd, MMMM Do, YYYY') }}</span>
-                </p>
-                <p class="time">
-                  <span class="material-symbols-outlined">schedule</span>
-                  
-                  <span class="orange-text">{{ $moment(`${$moment().format('YYYY-MM-DD')} ${event.start_time}`).format('h:mm a') }}&nbsp;&mdash;&nbsp;{{ $moment(`${$moment().format('YYYY-MM-DD')} ${event.end_time}`).format('h:mm a') }}</span>
-                </p>
-                
-                  <div class="seeking-for" v-if="(userRole === 'artists' || userRole === 'organizer') && event.look_types.length > 0" >
-                    <h6 class="title">Seeking for</h6>
-                    <span class="badge type-artist" v-for="(look, index) in event.look_types" :key="index">{{ look }}</span>
-                  </div>
-                
-                <div v-if="isLoggedIn">
-                  <button class="btn btn-primary view-details" data-bs-toggle="modal" data-bs-target="#eventDetailsModal">View Details</button>
-                  <viewEventDetails />
-                </div>
-                <div v-else>
-                  <button class="btn btn-primary view-details" data-bs-toggle="modal" data-bs-target="#mustSignUp">View Details</button>
-                <signupmodal />
-                </div>
-
-                <button class="btn btn-primary send-proposal" @click="$router.push('/proposal')" v-if="userRole === 'artists'" >Send Proposal</button>
-              </div>
-            </div>
+            <event-card :event="event" :pos="index" @show="toggleEvent" />
           </div>
         </div>                                                   
         <div class="row" v-else>
@@ -148,7 +101,10 @@
         </div>
       </div>
     </section>
-    <signupmodal />
+    
+    <view-detail v-if="isLoggedIn" />
+    <signupmodal v-else />
+
     <events-modal @close="dismiss"/>
     <event-success />
     <faq />
@@ -163,8 +119,9 @@ import Layout from '/src/components/Layouts/Layout.vue';
 import Faq from '/src/components/Home/FAQ.vue';
 import MustSignupModal from '/src/components/Artist/MustSignupModal.vue';
 import EventsModal from '/src/components/Auth/Events/Modal.vue';
-import ViewEventDetailsModal from '/src/components/Events/ViewEventDetailsModal.vue';
+import ViewDetail from '/src/components/Events/ViewEventDetailsModal.vue';
 import EventSuccess from '/src/components/Auth/Events/SuccessModal.vue';
+import EventCard from '/src/components/Events/Card.vue';
 
 export default {
   components: {
@@ -172,9 +129,10 @@ export default {
     faq: Faq,
     signupmodal: MustSignupModal,
     EventsModal,
-    viewEventDetails: ViewEventDetailsModal,
+    ViewDetail,
     EventSuccess,
     Multiselect,
+    EventCard,
   },
   setup()
   {
@@ -183,125 +141,6 @@ export default {
   data()
   {
     return {
-      // events: [
-      //   {
-      //     id: 1,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687423572/available-event-bg-1_p0gkss.webp',
-      //     name: 'Sneaker Con Bay Area',
-      //     description: 'Lorem ipsum dolor sit amet consectetur. Sit suscipit natoque molestie nulla orci. Quam bibendum euismod consequat facilisis. Sit suscipit natoque molestie nulla orci',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: true,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'KC Event Organizer',
-      //     organizerImage: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687491614/organized-by-logo_ybzrzs.svg'
-      //   },
-      //   {
-      //     id: 2,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687423571/available-event-bg-2_glbbpx.webp',
-      //     name: 'SM City Naga',
-      //     description: 'Sziget is a massive island festival in Budapest. Today, it’s one of Europe’s biggest and best music festivals.',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: false,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'Naga LGU',
-      //     organizerImage: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687754799/diamond_tnvz7y.webp'
-      //   },
-      //   {
-      //     id: 3,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687423572/available-event-bg-3_c1uxu3.webp',
-      //     name: 'Legazpi Sawangan Park',
-      //     description: 'Coachella is a common household name in music events. This is perhaps the most famous festival in the world, and also one of the greatest events',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: false,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'Facade Hotel',
-      //     organizerImage: 'https://yt3.ggpht.com/yti/AHyvSCDjcNUmq7Ei-mklDFbjyPOH83Rgwhzg9eoFXRAgXA=s88-c-k-c0x00ffffff-no-rj-mo'
-      //   },
-      //   {
-      //     id: 4,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1686465779/samples/cloudinary-group.jpg',
-      //     name: 'SM City Legazpi',
-      //     description: 'Lorem ipsum dolor sit amet consectetur. Sit suscipit natoque molestie nulla orci. Quam bibendum euismod consequat facilisis...',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: false,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'KC Event Organizer',
-      //     organizerImage: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687491614/organized-by-logo_ybzrzs.svg'
-      //   },
-      //   {
-      //     id: 5,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687423571/available-event-bg-2_glbbpx.webp',
-      //     name: 'Albay Astrodome',
-      //     description: 'Lorem ipsum dolor sit amet consectetur. Sit suscipit natoque molestie nulla orci. Quam bibendum euismod consequat facilisis...',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: false,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'KC Event Organizer',
-      //     organizerImage: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687491614/organized-by-logo_ybzrzs.svg'
-      //   },
-      //   {
-      //     id: 6,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687423572/available-event-bg-3_c1uxu3.webp',
-      //     name: 'Bagasbas Beach',
-      //     description: 'Lorem ipsum dolor sit amet consectetur. Sit suscipit natoque molestie nulla orci. Quam bibendum euismod consequat facilisis...',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: true,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'KC Event Organizer',
-      //     organizerImage: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687491614/organized-by-logo_ybzrzs.svg'
-      //   },
-      //   {
-      //     id: 7,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1686465779/samples/cloudinary-group.jpg',
-      //     name: 'SM City Legazpi',
-      //     description: 'Lorem ipsum dolor sit amet consectetur. Sit suscipit natoque molestie nulla orci. Quam bibendum euismod consequat facilisis...',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: false,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'KC Event Organizer',
-      //     organizerImage: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687491614/organized-by-logo_ybzrzs.svg'
-      //   },
-      //   {
-      //     id: 8,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687423571/available-event-bg-2_glbbpx.webp',
-      //     name: 'Albay Astrodome',
-      //     description: 'Lorem ipsum dolor sit amet consectetur. Sit suscipit natoque molestie nulla orci. Quam bibendum euismod consequat facilisis...',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: false,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'KC Event Organizer',
-      //     organizerImage: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687491614/organized-by-logo_ybzrzs.svg'
-      //   },
-      //   {
-      //     id: 9,
-      //     image: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687423572/available-event-bg-3_c1uxu3.webp',
-      //     name: 'Bagasbas Beach',
-      //     description: 'Lorem ipsum dolor sit amet consectetur. Sit suscipit natoque molestie nulla orci. Quam bibendum euismod consequat facilisis...',
-      //     location: 'Naga City, Cam. Sur Philippines',
-      //     date: 'Saturday, June 12,2023',
-      //     time: '8:00pm - 12:00 pm',
-      //     cost: true,
-      //     typeOfEvent: ['Music Festival Events', 'Concert'],
-      //     organizer: 'KC Event Organizer',
-      //     organizerImage: 'https://res.cloudinary.com/daorvtlls/image/upload/v1687491614/organized-by-logo_ybzrzs.svg'
-      //   }
-      // ]
 
     }
   },
@@ -345,6 +184,24 @@ export default {
     ...mapMutations([
       'RESET_EVENT_FILTER'
     ]),
+    toggleEvent(pos)
+    {
+      if (this.isLoggedIn) {
+
+        new Modal(document.getElementById('eventDetailsModal'), {
+          keyboard: false,
+          backdrop: 'static',
+        }).show();
+
+      } else {
+
+        new Modal(document.getElementById('mustSignUp'), {
+          keyboard: false,
+          backdrop: 'static',
+        }).show();
+
+      }
+    },
     openModal(data){
         this.$root.$emit("bv::show::modal", "#mustSignUp");
     },
