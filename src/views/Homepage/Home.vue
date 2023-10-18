ccb1a7d08d9b09eebf9e4d628224c84a660feb08
Wolf Syntax <jaysonalpe@gmail.com>
2023-10-18 19:55:12 +0800

    Update for SEO Meta - debug

--- src/views/Homepage/Home.vue
+++ src/views/Homepage/Home.vue
@@ -31,64 +31,32 @@ export default {
       title: 'Home',
       meta: [
         {
-          // hid: 'og-title',
           property: 'og:title', 
           content: 'Geebu',
         },
         {
-          // hid: 'og-description',
           property: 'og:description', 
           content: 'Experience the Ultimate Fusion of Creativity and Entertainment with Geebu Create Your Custom Song, Tailored Exclusively for Your Special Event or Party.',
         },
         {
-          // hid: 'og-image',
           property: 'og:image',
-          content: 'https://develop.geebu.ph/assets/home/home-slider-1.webp',
+          content: 'https://picsum.photos/1200/67',
         },
         {
-          // hid: 'og-type',
-          property: 'og:type',
-          content: 'website',
-        },
-        // {
-        //   // hid: 'og-image-type',
-        //   property: 'og:image:type',
-        //   content: 'image/webp',
-        // },
-        // {
-        //   // hid: 'og-image-width',
-        //   property: 'og:image:width',
-        //   content: '1024',
-        // },
-        // {
-        //   // hid: 'og-image-height',
-        //   property: 'og:image:height',
-        //   content: '1024',
-        // },
-        {
-          // hid: 'twitter-card',
           name: 'twitter:card',
-          content: 'summary',
+          content: 'summary_large_image',
         },
         {
-          // hid: 'twitter-title',
           name: 'twitter:title',
           content: 'Geebu',
         },
         {
-          // hid: 'twitter-description',
           name: 'twitter:description',
           content: 'Experience the Ultimate Fusion of Creativity and Entertainment with Geebu Create Your Custom Song, Tailored Exclusively for Your Special Event or Party.',
         },
         {
-          // hid: 'twitter-image',
           name: 'twitter:image',
-          content: 'https://develop.geebu.ph/assets/geebu-logo.svg',
-        },
-        {
-          // hid: 'twitter-url',
-          name: 'twitter:url',
-          content: 'https://develop.geebu.ph',
+          content: 'https://picsum.photos/1200/67',
         },
       ],
      
