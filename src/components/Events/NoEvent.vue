701ba46ebd81db79eff6a0fb3de4e9ea1b6fed50
johnmarkbondad <johnmarkbondad333@gmail.com>
2023-11-16 17:46:35 +0800

    design no event

---
+++ src/components/Events/NoEvent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="no-event-posted">
+        <div class="no-event-wrap">
+            <svg class="no-event-icon" xmlns="http://www.w3.org/2000/svg" width="58" height="64" viewBox="0 0 58 64" fill="none">
+            <path d="M47.108 23.0875H10.9656L3.625 37.625V55.7688H54.443V37.625L47.108 23.0875ZM35.491 37.7875C35.491 41.7188 32.6023 44.9063 29.034 44.9063C25.4656 44.9063 22.577 41.7188 22.577 37.7875H6.65527L12.5346 25.4125H45.5277L51.407 37.7875H35.491ZM28.4449 8.1875H30.6596V16.975H28.4449V8.1875ZM13.4156 12.5501L14.9816 10.8222L20.6127 17.0358L19.0467 18.7638L13.4156 12.5501ZM37.8103 17.0398L43.4414 10.8261L45.0074 12.5541L39.3763 18.7678L37.8103 17.0398Z" fill="#ABADC6"/>
+        </svg>
+        <h6 class="mb-0 no-event">No event posted</h6>
+        </div>
+    </div>
+  </template>
+  
+  <script >
+  import { mapActions, mapState } from 'vuex';
+  
+export default {
+props: {
+},
+data: () => ({
+    
+}),
+methods: {
+
+},
+mounted() {
+},
+}
+</script>
+  
+<style lang="scss" scoped>
+</style>
