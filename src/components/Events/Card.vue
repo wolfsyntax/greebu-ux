1178c0d34083aed0701f0f8d7636613a31b67c45
Wolf Syntax <jaysonalpe@gmail.com>
2023-12-07 12:31:54 +0800

    Truncate text for events seeking for

--- src/components/Events/Card.vue
+++ src/components/Events/Card.vue
@@ -38,14 +38,14 @@
         <div class="col">
           <div class="seeking-for" v-if="(userRole === 'artists' || userRole === 'organizer') && event.look_types.length > 0" >
             <h6 class="title">Seeking for</h6>
-            <span class="badge type-artist" v-for="(look, index) in event.look_types.slice(0,1)" :key="index">{{ look }}</span>
+            <span class="badge type-artist" v-for="(look, index) in event.look_types.slice(0,1)" :key="index">{{ look.substring(0,9) }}...</span>
             <span class="badge type-artist bg-transparent" v-if="event.look_types.length > 1">...</span>
           </div>
 
           <!-- set the opacity to 0 on this div when the created event has no Seeking for -->
           <div class="seeking-for" v-if="(userRole === 'artists' || userRole === 'organizer') && event.look_types.length === 0" >
             <h6 class="title" style="opacity: 0;">Seeking for</h6>
-            <span class="badge type-artist" style="opacity: 0;">acoustic band</span>
+            <span class="badge type-artist" style="opacity: 0;">{{ 'acoustic band'.substring(0,9)}}...</span>
           </div>
         </div>
 
