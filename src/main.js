228d77db307b8510235adad6b7b7d58e436ba620
wolfsyntax <jaysonalpe@gmail.com>
2023-12-27 10:37:07 +0800

    Integrate eslint (#384)

--- src/main.js
+++ src/main.js
@@ -1,36 +1,64 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 
-import { createApp } from "vue";
+import { createApp } from 'vue'
 
-import axios from "axios";
-import VueAxios from "vue-axios";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 // import Vuesax from 'vuesax'
 
 // import Multiselect  from '@vueform/multiselect'
-import "@vueform/multiselect/themes/default.css";
+import '@vueform/multiselect/themes/default.css'
 
-import router from "./router";
-import i18n from "./plugins/i18n";
-import store from "./store";
-import "bootstrap";
+import router from './router'
+import i18n from './plugins/i18n'
+import store from './store'
+import 'bootstrap'
 
-import App from "./App.vue";
+import App from './App.vue'
 
 // Fontawesome Icons
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faUserSecret, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, faTrash } from '@fortawesome/free-solid-svg-icons'
 import {
   faTwitter,
   faYoutube,
   faSpotify,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons'
+
+import jQuery from 'jquery'
+import moment from 'moment'
+
+import './style.css'
+import './vuesax.css'
+// import 'vuesax/dist/vuesax.css';
+
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './firebase/init'
+
+import Pusher from './plugins/pusher'
+import { Cropper } from 'vue-advanced-cropper'
+import 'vue-advanced-cropper/dist/style.css'
+import TreeItem from './components/Organizer/TreeItem.vue'
+
+import { createHead } from '@vueuse/head'
+
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+import Compressor from 'compressorjs'
+// import CircleImage from './components/CircleImage.vue'
+
+import AudioPlayer from '@liripeng/vue-audio-player'
+
+import 'bootstrap/dist/js/bootstrap.js'
 
 library.add(
   faUserSecret,
@@ -39,34 +67,8 @@ library.add(
   faYoutube,
   faSpotify,
   faInstagram
-);
-
-import jQuery from "jquery";
-import moment from "moment";
-
-import "./style.css";
-import "./vuesax.css";
-// import 'vuesax/dist/vuesax.css';
-
-import { VueFire, VueFireAuth } from "vuefire";
-import { firebaseApp } from "./firebase/init";
-
-import Pusher from "./plugins/pusher";
-import { Cropper } from "vue-advanced-cropper";
-import "vue-advanced-cropper/dist/style.css";
-import TreeItem from "./components/Organizer/TreeItem.vue";
-
-import { createHead } from "@vueuse/head";
-const head = createHead();
-
-import FullCalendar from "@fullcalendar/vue3";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
-
-import Compressor from 'compressorjs';
-//import CircleImage from './components/CircleImage.vue'
-
-import AudioPlayer from '@liripeng/vue-audio-player';
+)
+const head = createHead()
 
 const app = createApp(App)
   .use(i18n)
@@ -87,52 +89,50 @@ const app = createApp(App)
     apiKey: import.meta.env.VITE_PUSHER_APP_KEY,
     appId: import.meta.env.VITE_PUSHER_APP_ID,
     secret: import.meta.env.VITE_PUSHER_APP_SECRET,
-    cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+    cluster: import.meta.env.VITE_PUSHER_CLUSTER
   })
   .use(VueFire, {
     // imported above but could also just be created here
     firebaseApp,
     modules: [
       // we will see other modules later on
-      VueFireAuth(),
-    ],
+      VueFireAuth()
+    ]
   })
   .use({
     install: function (Vue, options) {
-      Vue.prototype.$jQuery = jQuery; // you'll have this.$jQuery anywhere in your vue project
-    },
+      Vue.prototype.$jQuery = jQuery // you'll have this.$jQuery anywhere in your vue project
+    }
   })
   // .use(BootstrapVueIcons)
   // .component('multiselect', Multiselect)
-  .component("compressor", Compressor)
-  .component("cropper", Cropper)
-  .component("font-awesome-icon", FontAwesomeIcon)
-  .component("tree-item", TreeItem)
-  //.component("circle-image", CircleImage)
-  .component("v-calendar", FullCalendar)
-  .component("n-progress", NProgress)
+  .component('compressor', Compressor)
+  .component('cropper', Cropper)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .component('tree-item', TreeItem)
+  // .component("circle-image", CircleImage)
+  .component('v-calendar', FullCalendar)
+  .component('n-progress', NProgress)
 
-app.config.globalProperties.$moment = moment;
+app.config.globalProperties.$moment = moment
 app.config.globalProperties.$filters = {
-  diffForHumans(date) {
-    if (!date) date = moment();
+  diffForHumans (date) {
+    if (!date) date = moment()
 
-    var text = moment.duration(moment().diff(moment(date))).humanize();
+    let text = moment.duration(moment().diff(moment(date))).humanize()
 
-    if (!text.includes("ago")) text = `${text} ago`;
+    if (!text.includes('ago')) text = `${text} ago`
 
-    return text;
+    return text
   },
-  timeAgo(date) {
-    return moment(date).fromNow();
+  timeAgo (date) {
+    return moment(date).fromNow()
   },
-  timer(sec) {
-    return moment.utc(sec * 1000).format("mm:ss");
-  },
-};
+  timer (sec) {
+    return moment.utc(sec * 1000).format('mm:ss')
+  }
+}
 
-app.config.globalProperties.$assets = import.meta.env.VITE_ASSET_URL;
+app.config.globalProperties.$assets = import.meta.env.VITE_ASSET_URL
 
-app.mount("#app");
-
-import "bootstrap/dist/js/bootstrap.js";
+app.mount('#app')
--- src/store/modules/organizers/actions.js
+++ src/store/modules/organizers/actions.js
@@ -1,379 +1,379 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const fetchOrganizerOptions = (
   { commit, rootState, state },
   payload
 ) => {
   return new Promise((resolve, reject) => {
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + (rootState.bearerToken || localStorage.api_token);
-    console.log("Fetch Organizer Options: ", payload, rootState.bearerToken);
+    axios.defaults.headers.common.Authorization =
+      'Bearer ' + (rootState.bearerToken || localStorage.api_token)
+    console.log('Fetch Organizer Options: ', payload, rootState.bearerToken)
     setTimeout(async () => {
       await axios
         .get(
           `${
-            import.meta.env.VITE_BASE_URL || "http://localhost:8000"
+            import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
           }/api/organizer/forms`,
           payload
         )
         .then((response) => {
-          console.log("\n\nOrganizer Form: ", response);
+          console.log('\n\nOrganizer Form: ', response)
 
           const {
             status: statusCode,
-            data: { message, status, result },
-          } = response;
+            data: { message, status, result }
+          } = response
 
           if (statusCode === 200 && status === 200) {
-            const { event_types, staff_roles } = result;
+            const { event_types, staff_roles } = result
 
-            if (event_types) commit("SET_EVENT_TYPES", event_types);
-            if (staff_roles) commit("SET_STAFF_ROLES", staff_roles);
+            if (event_types) commit('SET_EVENT_TYPES', event_types)
+            if (staff_roles) commit('SET_STAFF_ROLES', staff_roles)
           }
 
-          resolve(response);
+          resolve(response)
         })
         .catch((err) => {
-          const { data, status: statusCode } = err;
+          const { data, status: statusCode } = err
 
-          reject(data);
-        });
-    }, 1500);
-  });
-};
+          reject(data)
+        })
+    }, 1500)
+  })
+}
 
 export const fetchStaff = ({ commit, rootState, state }, payload) => {
   return new Promise((resolve, reject) => {
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + (rootState.bearerToken || localStorage.api_token);
+    axios.defaults.headers.common.Authorization =
+      'Bearer ' + (rootState.bearerToken || localStorage.api_token)
 
     setTimeout(async () => {
       await axios
         .get(
           `${
-            import.meta.env.VITE_BASE_URL || "http://localhost:8000"
+            import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
           }/api/organizer/staff`,
           payload
         )
         .then((response) => {
           const {
             status: statusCode,
-            data: { status, result, message },
-          } = response;
+            data: { status, result, message }
+          } = response
 
           if (statusCode === 200 && status === 200) {
-            const { organizer, member, members } = result;
-            commit("SET_STAFF", members);
+            const { organizer, member, members } = result
+            commit('SET_STAFF', members)
 
-            resolve({ status: statusCode, message, result: result });
+            resolve({ status: statusCode, message, result })
           }
 
-          reject({ message, status: statusCode, result });
+          reject({ message, status: statusCode, result })
         })
         .catch((err) => {
-          const { data, status: statusCode } = err;
+          const { data, status: statusCode } = err
 
-          reject(data);
-        });
-    }, 1500);
-  });
-};
+          reject(data)
+        })
+    }, 1500)
+  })
+}
 
 export const addStaff = ({ commit, rootState, state }, payload) => {
   return new Promise((resolve, reject) => {
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + (rootState.bearerToken || localStorage.api_token);
+    axios.defaults.headers.common.Authorization =
+      'Bearer ' + (rootState.bearerToken || localStorage.api_token)
 
     setTimeout(async () => {
       await axios
         .post(
           `${
-            import.meta.env.VITE_BASE_URL || "http://localhost:8000"
+            import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
           }/api/organizer/staff`,
           payload,
           {
             headers: {
-              "Content-Type": "multipart/form-data",
-            },
+              'Content-Type': 'multipart/form-data'
+            }
           }
         )
         .then((response) => {
           const {
             status: statusCode,
-            data: { status, result, message },
-          } = response;
+            data: { status, result, message }
+          } = response
 
           if (statusCode === 200 && status === 200) {
-            const { organizer, member, members } = result;
-            commit("SET_STAFF", members);
-            resolve({ status: statusCode, message, result: result });
+            const { organizer, member, members } = result
+            commit('SET_STAFF', members)
+            resolve({ status: statusCode, message, result })
           }
 
-          reject({ message, status: statusCode, result });
+          reject({ message, status: statusCode, result })
         })
         .catch((err) => {
           const {
             status: statusCode,
-            data: { status, result, message },
-          } = err;
+            data: { status, result, message }
+          } = err
 
           if (status === 422) {
-            reject({ status, message, result: result?.errors || [] });
+            reject({ status, message, result: result?.errors || [] })
           } else {
-            reject({ status, message, result: result });
+            reject({ status, message, result })
           }
-        });
-    }, 1000);
-  });
-};
+        })
+    }, 1000)
+  })
+}
 
 export const editStaff = ({ commit, rootState, state }, payload) => {
   return new Promise((resolve, reject) => {
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + (rootState.bearerToken || localStorage.api_token);
-    var url = `${
-      import.meta.env.VITE_BASE_URL || "http://localhost:8000"
-    }/api/organizer/staff/${payload.id}/edit`;
-    console.log("Target URL - Staff: ", url);
+    axios.defaults.headers.common.Authorization =
+      'Bearer ' + (rootState.bearerToken || localStorage.api_token)
+    const url = `${
+      import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
+    }/api/organizer/staff/${payload.id}/edit`
+    console.log('Target URL - Staff: ', url)
 
     setTimeout(async () => {
       await axios
         .post(url, payload, {
           headers: {
-            "Content-Type": "multipart/form-data",
-          },
+            'Content-Type': 'multipart/form-data'
+          }
         })
         .then((response) => {
           const {
             status: statusCode,
-            data: { status, result, message },
-          } = response;
-          console.log("Edit Staff response: ", response);
+            data: { status, result, message }
+          } = response
+          console.log('Edit Staff response: ', response)
           if (statusCode === 200 && status === 200) {
-            const { organizer, member, members } = result;
-            console.log("Success Response: ", members);
+            const { organizer, member, members } = result
+            console.log('Success Response: ', members)
 
-            commit("SET_STAFF", members);
-            resolve({ status: statusCode, message, result: result });
+            commit('SET_STAFF', members)
+            resolve({ status: statusCode, message, result })
           }
 
-          reject({ message, status: statusCode, result });
+          reject({ message, status: statusCode, result })
         })
         .catch((err) => {
           const {
             status: statusCode,
-            data: { status, result, message },
-          } = err;
+            data: { status, result, message }
+          } = err
 
           if (status === 422) {
-            reject({ status, message, result: result?.errors || [] });
+            reject({ status, message, result: result?.errors || [] })
           } else {
-            reject({ status, message, result: result });
+            reject({ status, message, result })
           }
-        });
-    }, 1000);
-  });
-};
+        })
+    }, 1000)
+  })
+}
 
 export const removeStaff = ({ commit, rootState, state }, payload) => {
   return new Promise((resolve, reject) => {
-    state.staff = state.staff.filter((item) => item.id !== payload);
+    state.staff = state.staff.filter((item) => item.id !== payload)
     // .filter((item) => item !== index)
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + (rootState.bearerToken || localStorage.api_token);
+    axios.defaults.headers.common.Authorization =
+      'Bearer ' + (rootState.bearerToken || localStorage.api_token)
 
     setTimeout(() => {
       axios
         .delete(
           `${
-            import.meta.env.VITE_BASE_URL || "http://localhost:8000"
+            import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
           }/api/organizer/staff/${payload}`
         )
         .then((response) => {
-          console.log("\n\nRemove Member Response: ", response);
+          console.log('\n\nRemove Member Response: ', response)
 
           const {
-            data: { result, status },
-          } = response;
+            data: { result, status }
+          } = response
 
           if (status === 200) {
-            commit("SET_MEMBERS", result.members);
+            commit('SET_MEMBERS', result.members)
           }
 
-          resolve(response.data);
+          resolve(response.data)
         })
         .catch((err) => {
-          const { data, status: statusCode } = err;
+          const { data, status: statusCode } = err
 
-          reject(data);
-        });
-    }, 1000);
-  });
-};
+          reject(data)
+        })
+    }, 1000)
+  })
+}
 //
 
 export const fetchArtistProposal = ({ commit, rootState, state }, payload) => {
   return new Promise((resolve, reject) => {
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + (rootState.bearerToken || localStorage.api_token);
+    axios.defaults.headers.common.Authorization =
+      'Bearer ' + (rootState.bearerToken || localStorage.api_token)
 
-    var filter = payload?.filterBy ?? "pending";
+    const filter = payload?.filterBy ?? 'pending'
 
     setTimeout(() => {
       axios
         .get(
           `${
-            import.meta.env.VITE_BASE_URL || "http://localhost:8000"
+            import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
           }/api/artist-proposal?role=organizer&search=${
-            payload?.search || ""
-          }&sortBy=${payload?.sortBy || "DESC"}&filterBy=${filter}`
+            payload?.search || ''
+          }&sortBy=${payload?.sortBy || 'DESC'}&filterBy=${filter}`
         )
         .then((response) => {
           const {
             data: { status, result },
-            status: statusCode,
-          } = response;
+            status: statusCode
+          } = response
 
           if (statusCode === 200 && status === 200) {
             console.log(
-              "URL Request: ",
+              'URL Request: ',
               `/api/artist-proposal?role=organizer&search=${
-                payload?.search || ""
-              }&sortBy=${payload?.sortBy || "DESC"}&filterBy=${filter}`
-            );
+                payload?.search || ''
+              }&sortBy=${payload?.sortBy || 'DESC'}&filterBy=${filter}`
+            )
 
-            if (filter === "accepted") {
+            if (filter === 'accepted') {
               commit(
-                "SET_PERFORMER_ACCEPTED_PROPOSAL",
+                'SET_PERFORMER_ACCEPTED_PROPOSAL',
                 result?.proposals || []
-              );
-            } else if (filter === "declined") {
+              )
+            } else if (filter === 'declined') {
               commit(
-                "SET_PERFORMER_DECLINED_PROPOSAL",
+                'SET_PERFORMER_DECLINED_PROPOSAL',
                 result?.proposals || []
-              );
+              )
             } else {
-              commit("SET_PERFORMER_PENDING_PROPOSAL", result?.proposals || []);
+              commit('SET_PERFORMER_PENDING_PROPOSAL', result?.proposals || [])
             }
           }
         })
         .catch((err) => {
-          const { data, status: statusCode } = err;
+          const { data, status: statusCode } = err
 
-          reject(data);
-        });
-    }, 1500);
-  });
-};
+          reject(data)
+        })
+    }, 1500)
+  })
+}
 
 export const acceptProposal = (
   { commit, rootState, state, dispatch },
   proposalId
 ) => {
   return new Promise((resolve, reject) => {
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + (rootState.bearerToken || localStorage.api_token);
+    axios.defaults.headers.common.Authorization =
+      'Bearer ' + (rootState.bearerToken || localStorage.api_token)
 
-    if (!proposalId) resolve();
+    if (!proposalId) resolve()
 
     setTimeout(() => {
       axios
         .post(
           `${
-            import.meta.env.VITE_BASE_URL || "http://localhost:8000"
+            import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
           }/api/organizer/${proposalId}/accept-proposal`
         )
         .then((response) => {
           const {
             data: { status, result },
-            status: statusCode,
-          } = response;
-          console.log("[Actions] accept proposal: ", response);
+            status: statusCode
+          } = response
+          console.log('[Actions] accept proposal: ', response)
 
           if (statusCode === 200 && status === 200) {
-            dispatch("fetchArtistProposal", {
-              search: "",
-              sortBy: "DESC",
-              filterBy: "pending",
-            });
+            dispatch('fetchArtistProposal', {
+              search: '',
+              sortBy: 'DESC',
+              filterBy: 'pending'
+            })
 
-            dispatch("fetchArtistProposal", {
-              search: "",
-              sortBy: "DESC",
-              filterBy: "accepted",
-            });
+            dispatch('fetchArtistProposal', {
+              search: '',
+              sortBy: 'DESC',
+              filterBy: 'accepted'
+            })
 
-            dispatch("fetchArtistProposal", {
-              search: "",
-              sortBy: "DESC",
-              filterBy: "declined",
-            });
+            dispatch('fetchArtistProposal', {
+              search: '',
+              sortBy: 'DESC',
+              filterBy: 'declined'
+            })
 
-            resolve(response?.data);
+            resolve(response?.data)
           }
 
-          reject(response?.data);
+          reject(response?.data)
         })
         .catch((err) => {
-          const { data, status: statusCode } = err;
+          const { data, status: statusCode } = err
 
-          reject(data);
-        });
-    }, 1000);
-  });
-};
+          reject(data)
+        })
+    }, 1000)
+  })
+}
 
 export const declineProposal = (
   { commit, rootState, state, dispatch },
   payload
 ) => {
   return new Promise((resolve, reject) => {
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + (rootState.bearerToken || localStorage.api_token);
+    axios.defaults.headers.common.Authorization =
+      'Bearer ' + (rootState.bearerToken || localStorage.api_token)
 
-    if (!payload) resolve();
+    if (!payload) resolve()
 
     setTimeout(() => {
       axios
         .post(
           `${
-            import.meta.env.VITE_BASE_URL || "http://localhost:8000"
+            import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
           }/api/organizer/${payload}/decline-proposal`
         )
         .then((response) => {
           const {
             data: { status, result },
-            status: statusCode,
-          } = response;
+            status: statusCode
+          } = response
 
           if (statusCode === 200 && status === 200) {
-            dispatch("fetchArtistProposal", {
-              search: "",
-              sortBy: "DESC",
-              filterBy: "pending",
-            });
+            dispatch('fetchArtistProposal', {
+              search: '',
+              sortBy: 'DESC',
+              filterBy: 'pending'
+            })
 
-            dispatch("fetchArtistProposal", {
-              search: "",
-              sortBy: "DESC",
-              filterBy: "accepted",
-            });
+            dispatch('fetchArtistProposal', {
+              search: '',
+              sortBy: 'DESC',
+              filterBy: 'accepted'
+            })
 
-            dispatch("fetchArtistProposal", {
-              search: "",
-              sortBy: "DESC",
-              filterBy: "declined",
-            });
+            dispatch('fetchArtistProposal', {
+              search: '',
+              sortBy: 'DESC',
+              filterBy: 'declined'
+            })
 
-            resolve(response?.data);
+            resolve(response?.data)
           }
-          reject(response.data);
+          reject(response.data)
         })
         .catch((err) => {
-          const { data, status: statusCode } = err;
+          const { data, status: statusCode } = err
 
-          reject(data);
-        });
-    }, 1000);
-  });
-};
+          reject(data)
+        })
+    }, 1000)
+  })
+}
