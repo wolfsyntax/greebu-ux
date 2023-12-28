641613378051d8cbd4d58fd33169a04d25152af0
johnmarkbondad <johnmarkbondad333@gmail.com>
2023-12-28 19:48:52 +0800

    gsdfgdfgf

--- src/components/Dashboard/Tabs/NoUploaded/ComingSoon.vue
+++ src/components/Dashboard/Tabs/NoUploaded/ComingSoon.vue
@@ -1,16 +1,15 @@
 <template>
     <div id="coming-soon">
-
       <div class="text-center soon-wrap">
         <h2 class="coming">Coming soon..</h2>
         <p class="ready">Get ready for something exciting!</p>
         <button type="button" class="btn goto">Go to Homepage</button>
       </div>
-
     </div>
 </template>
 
 <script>
+
 export default {
   props: {
   },
@@ -18,6 +17,8 @@ export default {
     return {}
   },
   mounted () {
+  },
+  computed: {
   },
   methods: {
   },
