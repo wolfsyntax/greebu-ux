cede81a23fca6ba6e5730ee84efe353e3a3018d9
Michelle Panganiban <panganibanmish.personal@gmail.com>
2024-01-02 18:16:14 +0800

    add route in go to home page

--- src/components/Dashboard/Tabs/NoUploaded/ComingSoon.vue
+++ src/components/Dashboard/Tabs/NoUploaded/ComingSoon.vue
@@ -3,7 +3,7 @@
       <div class="text-center soon-wrap">
         <h2 class="coming">Coming soon..</h2>
         <p class="ready">Get ready for something exciting!</p>
-        <button type="button" class="btn goto">Go to Homepage</button>
+        <button type="button" @click="$router.push({name: 'home' })" class="btn goto">Go to Homepage</button>
       </div>
     </div>
 </template>
