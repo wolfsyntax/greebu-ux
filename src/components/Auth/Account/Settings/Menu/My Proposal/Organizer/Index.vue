ca058ba41f9d6365d9573defce8c4962bc429967
John Mark Bondad <43505912+john-mark-bondad@users.noreply.github.com>
2024-01-04 23:13:17 +0800

    show toast msg (#407)
    * show toast msg
    
    * fix hide toast when clicking the remove icon
    
    * code change
    
    * hide func
    
    * remove from loop

--- src/components/Auth/Account/Settings/Menu/My Proposal/Organizer/Index.vue
+++ src/components/Auth/Account/Settings/Menu/My Proposal/Organizer/Index.vue
@@ -1,13 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="">
     <div class="d-flex align-items-center justify-content-between title-wrapper">
       <h3 class="title">My Proposal</h3>
-      
     </div> <!-- end of title-wrapper -->
 
     <div class="d-flex align-items-center customized-songs-tabs">
-      <li class="d-flex align-items-center" 
-        v-for="proposal in myProposal" 
+      <li class="d-flex align-items-center"
+        v-for="proposal in myProposal"
         :key="proposal.name"
         :class="{ 'active-option': selectedCustomized === proposal.name }"
         @click="showCustomizeContent(proposal)"
@@ -15,91 +15,82 @@
         <h5 class="tab-title">{{ proposal.name }}</h5>
       </li>
     </div>
-    
     <pending-proposals v-if="selectedCustomized === 'Pending Proposals'"/>
     <accepted-proposals v-if="selectedCustomized === 'Accepted Proposals'"/>
     <denied-proposals v-if="selectedCustomized === 'Denied Proposals'"/>
-    
   </div>
 </template>
-  
+
 <script>
 
-import {mapActions, mapState} from 'vuex';
+import { mapActions } from 'vuex'
 
-import DeniedProposals from './DeniedProposals.vue';
-import AcceptedProposals from './AcceptedProposals.vue';
-import PendingProposals from './PendingProposals.vue';
+import DeniedProposals from './DeniedProposals.vue'
+import AcceptedProposals from './AcceptedProposals.vue'
+import PendingProposals from './PendingProposals.vue'
 
 export default {
   components: {
     DeniedProposals,
     AcceptedProposals,
-    PendingProposals, 
-    
+    PendingProposals
   },
   props: {
-
   },
-  data() {
+  data () {
     return {
       myProposal: [
-        { name: 'Pending Proposals'},
-        { name: 'Accepted Proposals'},
-        { name: 'Denied Proposals'},
+        { name: 'Pending Proposals' },
+        { name: 'Accepted Proposals' },
+        { name: 'Denied Proposals' }
       ],
-      selectedCustomized: 'Pending Proposals', 
+      selectedCustomized: 'Pending Proposals'
     }
-  },      
+  },
   computed: {
-    
   },
   methods: {
     ...mapActions([
       'fetchMyProposals'
     ]),
-    showCustomizeContent(option) {
+    showCustomizeContent (option) {
       console.log('showCustomizeContent: ', option)
-      this.selectedCustomized = option.name;
+      this.selectedCustomized = option.name
     },
-    closeModal(){
-      this.showModal = false;
+    closeModal () {
+      this.showModal = false
     },
-    onModalAccepted(){
-      this.showToast = true;
+    onModalAccepted () {
+      // this.showToast = true;
 
-      setTimeout(() => { this.showToast = false; }, 7000);
+      // setTimeout(() => { this.showToast = false; }, 7000);
 
-      this.showModal = false;
-    },
+      // this.showModal = false;
+    }
     // closeModal() {
     //   this.isModalOpen = false;
     //   this.showToastComponent = false;
     // },
   },
-  mounted() {
-    
-    this.fetchMyProposals();  
-    this.fetchMyProposals('accepted');  
-    this.fetchMyProposals('declined');  
-    
+  mounted () {
+    this.fetchMyProposals()
+    this.fetchMyProposals('accepted')
+    this.fetchMyProposals('declined')
   },
   watch: {
-    selectedCustomized(val) {
-      var payload = 'pending';
-      
-      if (['Accepted Proposals', 'Denied Proposals', 'Submitted Proposals', ].includes(val)) {
-        
-        if (val === 'Accepted Proposals') payload = 'accepted';
-        else if (val=== 'Denied Proposals') payload = 'declined';
+    selectedCustomized (val) {
+      let payload = 'pending'
+
+      if (['Accepted Proposals', 'Denied Proposals', 'Submitted Proposals'].includes(val)) {
+        if (val === 'Accepted Proposals') payload = 'accepted'
+        else if (val === 'Denied Proposals') payload = 'declined'
 
         this.fetchMyProposals(payload)
-        .then(res => console.log('Tab click option (response): ', res));
+          .then(res => console.log('Tab click option (response): ', res))
       }
-      
     }
   }
-};
+}
 </script>
 
 <style>
